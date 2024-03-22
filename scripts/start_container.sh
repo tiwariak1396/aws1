9eb68927c37dfb7c0cbff2ff22bbe6078da88f72
tiwariak1396 <63969637+tiwariak1396@users.noreply.github.com>
2024-03-22 23:28:49 +0000

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhishek80/simple-python-flask-app@sha256:336ca1e79dd7c6ce1f6ab4d6fb55c8a79a8d197e51bf7ae89caa365da821a863
+docker pull abhishek80/simple-python-flask-app
 
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishek80/simple-python-flask-app@sha256:336ca1e79dd7c6ce1f6ab4d6fb55c8a79a8d197e51bf7ae89caa365da821a863
+docker run -d -p 5000:5000 abhishek80/simple-python-flask-app
